0f675b1006e03126764ef932c073b61b36b310cd
Sangmoo <first_1st@naver.com>
2018-05-15 01:39:19 +0900

    Reply User

--- Bot Application/Dialogs/RootDialog.cs
+++ Bot Application/Dialogs/RootDialog.cs
@@ -22,8 +22,10 @@ namespace $safeprojectname$.Dialogs
             // calculate something for us to return
             int length = (activity.Text ?? string.Empty).Length;
 
+
             // return our reply to the user
             await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
 
             context.Wait(MessageReceivedAsync);
         }
